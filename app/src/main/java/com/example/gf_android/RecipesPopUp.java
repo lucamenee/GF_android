a6c844945ef8a5120af5344898a9eb91e0d68b09
Luca Meneghetti <126782219+lucamenee@users.noreply.github.com>
2024-11-29 11:10:47 +0100

    light mode only and recipes pop up "better" graphics

--- app/src/main/java/com/example/gf_android/RecipesPopUp.java
+++ app/src/main/java/com/example/gf_android/RecipesPopUp.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,32 +26,59 @@ public class RecipesPopUp  extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.recipes_popup_fragment_layout, container, false);
 
-        // close popup when button is clicked
-        view.findViewById(R.id.close_button).setOnClickListener(v -> dismiss());
-
         List<Ricetta> recipes = Api.suggestRecepies(idInventario);
 
-        if (recipes != null) {
-            TextView textView = view.findViewById(R.id.dialog_message);
 
-            // building msg to show
-            StringBuilder rec = new StringBuilder();
+        TextView existingTextViewName = view.findViewById(R.id.recipe_name);
+        TextView existingTextViewIngredients = view.findViewById(R.id.recipes_ingredients);
+
+        LinearLayout parentLayout = view.findViewById(R.id.pop_up_recipes);
+
+
+
+        if (recipes != null) {
+
             for (Ricetta r : recipes) {
                 Log.i("RecipesPopUp", r.nome_ricetta);
-                rec.append(r.nome_ricetta).append("\n");
+
+                TextView newTextViewName = new TextView(this.getContext());
+                TextView newTextViewIngredients = new TextView(this.getContext());
+                copyAttributes(existingTextViewName, newTextViewName);
+                copyAttributes(existingTextViewIngredients, newTextViewIngredients);
+                newTextViewName.setText(r.nome_ricetta);
+
+                StringBuilder rec = new StringBuilder();
                 for (Ricetta.Ingrediente i : r.ingredienti) {
-                    rec.append("\n").append(i.nome_alimento).append(": ").append(i.grammi).append("g");
+                    rec.append(i.nome_alimento).append(": ").append(i.grammi).append("g").append("\n");
                 }
-                rec.append("\n");
+                rec.deleteCharAt(rec.length() - 1);
+                newTextViewIngredients.setText(rec.toString());
+
+                parentLayout.addView(newTextViewName);
+                parentLayout.addView(newTextViewIngredients);
+
+                parentLayout.removeView(existingTextViewName);
+                parentLayout.removeView(existingTextViewIngredients);
             }
 
-
-            if (rec.length() > 0) {
-                textView.setText(rec.toString());
-            }
         }
 
+        Button closeButton = view.findViewById(R.id.close_button);
+        parentLayout.removeView(closeButton);
+        parentLayout.addView(closeButton); // so button is replaced on the bottom
+        // close popup when button is clicked
+        closeButton.setOnClickListener(v -> dismiss());
+
         return view;
+    }
+
+    private void copyAttributes(TextView existingTextView, TextView newTextView) {
+        newTextView.setText(existingTextView.getText());
+        newTextView.setTextSize(existingTextView.getTextSize());
+        newTextView.setTextColor(existingTextView.getTextColors());
+        newTextView.setTypeface(existingTextView.getTypeface(), existingTextView.getTypeface().getStyle());
+        newTextView.setLayoutParams(existingTextView.getLayoutParams());
+        newTextView.setTextSize(existingTextView.getTextSize());
     }
 
 
